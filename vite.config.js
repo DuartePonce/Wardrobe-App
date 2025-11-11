ba95b5ca2eee832c64268b749f378ceef2e14d8e
duarteponce <duarteponce@tecnico.ulisboa.pt>
2025-11-11 13:29:32 +0000

    FIX: Add correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Wardrobe-App/',
+  base: '/Wardrobe-App/', // <-- THIS IS THE FIX
   plugins: [react()],
 })
